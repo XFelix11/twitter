13b63920236d14362c28f26b71b67df647e27cc2
Fengxu Xie <fengxuxie@Fengxus-MBP.lan>
2023-08-26 19:18:12 -0400

    add memcached

--- friendships/services.py
+++ friendships/services.py
@@ -1,7 +1,33 @@
+from django.conf import settings
+from django.core.cache import caches
 from friendships.models import Friendship
+from twitter.cache import FOLLOWINGS_PATTERN
+
+cache = caches['testing'] if settings.TESTING else caches['default']
+
 
 
 class FriendshipService(object):
+
+    @classmethod
+    def get_following_user_id_set(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        user_id_set = cache.get(key)
+        if user_id_set is not None:
+            return user_id_set
+
+        friendships = Friendship.objects.filter(from_user_id=from_user_id)
+        user_id_set = set([
+            fs.to_user_id
+            for fs in friendships
+        ])
+        cache.set(key, user_id_set)
+        return user_id_set
+
+    @classmethod
+    def invalidate_following_cache(cls, from_user_id):
+        key = FOLLOWINGS_PATTERN.format(user_id=from_user_id)
+        cache.delete(key)
 
     @classmethod
     def get_followers(cls, user):
@@ -33,9 +59,11 @@ class FriendshipService(object):
         return [friendship.from_user for friendship in friendships]
     
     
-    @classmethod
-    def has_followed(cls, from_user, to_user):
-        return Friendship.objects.filter(
-            from_user=from_user,
-            to_user=to_user,
-        ).exists()
+    # @classmethod
+    # def has_followed(cls, from_user, to_user):
+    #     return Friendship.objects.filter(
+    #         from_user=from_user,
+    #         to_user=to_user,
+    #     ).exists()
+    
+
